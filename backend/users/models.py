2f1f157d40ec636cf59bfe4966ce81a00a686d56
jdulera <jdulera@bitglass.com>
2025-03-05 20:08:07 +0530

    Create user model and update settings

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    ROLES = (
+        ('SUPERADMIN', 'SuperAdmin'),
+        ('USER', 'User'),
+        ('GUEST', 'Guest'),
+    )
+    role = models.CharField(max_length=20, choices=ROLES, default='USER')
+    mfa_secret = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return self.username
